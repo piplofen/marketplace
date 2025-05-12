f0548c0d5d094272aa0d549508456dc03e92ca5b
piplofen <podkovyrov19@gmail.com>
2025-05-12 12:17:45 +0300

    123

--- marketplace/basic/views.py
+++ marketplace/basic/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    context = {
+        'title': 'Главная'
+    }
+    return render(request, "main.html", context=context)
+
+def items(request):
+    context = {
+        'title': 'Товары'
+    }
+    return render(request, 'items.html', context=context)
+
+def contacts(request):
+    context = {
+        'title': "Контакты"
+    }
+    return render(request, 'contacts.html', context=context)
+
+def about(request):
+    context = {
+        'title': 'О нас'
+    }
+    return render(request, "about.html", context=context)
+
+def pageNotFound(request, exception):
+    return HttpResponse("Unfortunately page not found(")
